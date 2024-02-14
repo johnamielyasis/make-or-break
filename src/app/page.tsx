97329129ed57c026979e0dea729a9b82bccab468
johnamielyasis <johnamielyasis@gmail.com>
2024-02-13 17:48:28 -0800

    feat: typography component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Typography } from "@/components";
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
         </p>
         <p className="font-sans">hw1 HELLOWORLD Overpass</p>
         <p className="font-sans2">hw2 HELLOWORLD Roboto</p>
+        <Typography variant="buttonText">button</Typography>
+        <Typography variant="copy">copy</Typography>
+        <Typography variant="nav">nav</Typography>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
